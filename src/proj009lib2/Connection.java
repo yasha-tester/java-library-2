565e7a0231b838089f3f16d44b7d15855b151a3a
yasha-tester <hemimoneycash@gmail.com>
2024-05-09 17:56:51 +0300

    list books on connection

--- src/proj009lib2/Connection.java
+++ src/proj009lib2/Connection.java
@@ -14,8 +14,34 @@ public class Connection {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/libraryDb", MyCreds.userName, MyCreds.pwd);
 		System.out.println("user " + MyCreds.userName + " is connected");
+		
+		Statement statement;
+		statement = conn.createStatement();
+		ResultSet resultSet;
+		resultSet = statement.executeQuery("select * from books");
+		
+		// here is
+		String code;
+		String title;
+		while(resultSet.next()) {
+			System.out.println("List of books :");
+			code = resultSet.getString("bookName");
+			title = resultSet.getString("bookAuthor").trim();
+			System.out.println("name: " + code + ";"
+					+ " author: " + title + ";");
+		}
+		// the code
+		
+		resultSet.close();
+		statement.close();
+		conn.close();
+		
 	} catch (Exception e) {
 		e.printStackTrace();
 	}
+
 }
+	public void check() {
+
+	}
 }
